7332a4da6769cb90185c12a2c1785338c464ebbd
Darya Karotchanka <daria.vyacheslavovna@gmail.com>
2021-04-29 01:31:36 +0200

    feat: add "donation" pop up.

--- online-zoo/pages/Landing/script.js
+++ online-zoo/pages/Landing/script.js
@@ -46,5 +46,152 @@ const getPos = function (current, active) {
 
 //*sliders PETS IN ZOO*//
 
+// *****POP UPS*****
+
+const coverElem = document.getElementById('cover');
+const formElem = document.getElementById('form-feedback');
+const donateButton = document.querySelector('.button4');
+const sendButton = document.getElementById('send');
+const amountField = document.getElementById('amount');
+const textField = document.getElementById('text-feedback');
+const exitButton = document.getElementById('close');
+
+
+const validate = () => {
+  if (
+    amountField.validity.valid &&
+    textField.validity.valid
+  ) {
+    sendButton.classList.remove('invalid');
+  } else {
+    sendButton.classList.add('invalid');
+  }
+}
+
+exitButton.addEventListener('click', () => {
+  document.body.classList.remove('notScrollable');
+  document.querySelector('.all').classList.remove('blur');
+  coverElem.classList.add('hidden');
+  formElem.classList.add('hidden');
+});
+
+donateButton.addEventListener('click', () => {
+  document.body.classList.add('notScrollable');
+  document.querySelector('.all').classList.add('blur');
+  coverElem.classList.remove('hidden');
+  formElem.classList.remove('hidden');
+});
+
+coverElem.addEventListener('click', () => {
+  document.body.classList.remove('notScrollable');
+  document.querySelector('.all').classList.remove('blur');
+  coverElem.classList.add('hidden');
+  formElem.classList.add('hidden');
+});
+
+sendButton.addEventListener('click', () => {
+  if (sendButton.classList.contains('invalid')) return;
+  document.body.classList.remove('notScrollable');
+  coverElem.classList.add('hidden');
+  formElem.classList.add('hidden');
+  document.querySelector('.all').classList.remove('blur');
+  window.open('../Donation/index.html', '_blank');
+  textField.reset();
+  amountField.reset();
+
+});
+
+amountField.addEventListener('input', () => {
+  validate();
+});
+
+textField.addEventListener('input', () => {
+  validate();
+});
+
+// ****CHOOSE ANIMAL FOR DONATION*****
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+/* Look for any elements with the class "custom-select": */
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /* For each element, create a new DIV that will act as the selected item: */
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /* For each element, create a new DIV that will contain the option list: */
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /* For each option in the original select element,
+    create a new DIV that will act as an option item: */
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        /* When an item is clicked, update the original select box,
+        and the selected item: */
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+    /* When the select box is clicked, close any other select boxes,
+    and open/close the current select box: */
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+
+function closeAllSelect(elmnt) {
+  /* A function that will close all select boxes in the document,
+  except the current select box: */
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+
+/* If the user clicks anywhere outside the select box,
+then close all select boxes: */
+document.addEventListener("click", closeAllSelect);
+
+// ****CURRENCY SELECTOR****
 
 
