356cb971f20c3421aee2800321ad436055da72f3
Darya Karotchanka <daria.vyacheslavovna@gmail.com>
2021-05-31 13:07:37 +0200

    feat: add "log out" btn.

--- match-match-game/src/components/registration/registration.ts
+++ match-match-game/src/components/registration/registration.ts
@@ -52,40 +52,46 @@ export class Registration extends BaseComponent {
 
     const closeBtn = this.element.getElementsByClassName('btn-cansel')[0];
     const fName = this.element.getElementsByClassName(
-      'first-name',
+      'first-name'
     )[0] as HTMLInputElement;
     const lName = this.element.getElementsByClassName(
-      'last-name',
+      'last-name'
     )[0] as HTMLInputElement;
     const email = this.element.getElementsByClassName(
-      'email',
+      'email'
     )[0] as HTMLInputElement;
     const addUserBtn = this.element.getElementsByClassName('validate')[0];
     let validEmail = false;
-    const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const reg =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    function closeRegist() {
+    function resetInputs() {
       document.getElementById('app')?.classList.toggle('blured');
-      document
-        .getElementsByClassName('registration')[0]
-        .classList.toggle('hidden');
       document.getElementsByClassName('cover')[0].classList.toggle('hidden');
-      document
-        .getElementsByClassName('regictration-btn')[0]
-        .classList.toggle('active');
       fName.classList.remove('not-validated-input');
       fName.classList.remove('validated-input');
       lName.classList.remove('not-validated-input');
       lName.classList.remove('validated-input');
       email.classList.remove('not-validated-input');
       email.classList.remove('validated-input');
+      addUserBtn.classList.add('unactive_btn');
+      document
+        .getElementsByClassName('regictration-btn')[0]
+        .classList.remove('active');
       fName.value = '';
       lName.value = '';
       email.value = '';
       const profPic = document.getElementsByClassName(
-        'thumb',
+        'thumb'
       )[0] as HTMLImageElement;
       profPic.src = `${ninja}`;
+    }
+
+    function closeRegist() {
+      document
+        .getElementsByClassName('registration')[0]
+        .classList.toggle('hidden');
+      resetInputs();
     }
 
     const validateInput = () => {
@@ -146,17 +152,12 @@ export class Registration extends BaseComponent {
         fName.value,
         lName.value,
         0,
-        this.avatar,
+        this.avatar
       );
       if ((await database.getUser(email.value)) === undefined) {
         await database.transaction(user);
       }
       game.activeUser = user;
-      setTimeout(() => {
-        alert(`Hello ${fName.value}, let's play a game!`);
-      }, 500);
-
-      document.getElementsByClassName('cover')[0].classList.toggle('hidden');
       document
         .getElementsByClassName('registration')[0]
         .classList.toggle('hidden');
@@ -167,14 +168,18 @@ export class Registration extends BaseComponent {
         .getElementsByClassName('start-game-btn')[0]
         .classList.remove('hidden');
       header.addProfPic();
+      document
+        .getElementsByClassName('log-out_btn')[0]
+        .classList.remove('hidden');
+      resetInputs();
     });
   }
 
   static displayImgData(
-    imgData: string | ArrayBuffer | null | undefined,
+    imgData: string | ArrayBuffer | null | undefined
   ): void {
     const profPic = document.getElementsByClassName(
-      'thumb',
+      'thumb'
     )[0] as HTMLImageElement;
     profPic.src = `${imgData}`;
     document
