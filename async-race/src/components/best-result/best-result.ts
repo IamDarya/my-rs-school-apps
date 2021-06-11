d1f8664bf1f5ff55dc310c808d79a630dee3c790
Darya Karotchanka <daria.vyacheslavovna@gmail.com>
2021-06-11 16:19:57 +0200

    fix: add small details to 'winners' page

--- async-race/src/components/best-result/best-result.ts
+++ async-race/src/components/best-result/best-result.ts
@@ -4,14 +4,20 @@ import { BaseComponent } from '../base-component';
 
 export class BestResult extends BaseComponent {
   router: Router;
+  numOfPageWinners: number;
 
   constructor(router: Router) {
     super('div', ['best-result-wrapper', 'hidden']);
+    this.numOfPageWinners = 1;
     this.router = router;
     this.element.innerHTML = `
         <div>
           <h2>Winners</h2>
-          <div class="amount-of-winners-on-page">(0)</div>
+          <div class="amount-of-winners-on-page"></div>
+          <div>
+            <p class="page-winners">Page #</p>
+            <div class="num-of-page-winners">${this.numOfPageWinners}</div>
+            </div>
           <ul>
             <li>
               <p>Position</p>
