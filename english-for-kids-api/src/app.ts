04968afc0857a1d75509b9435c433b6684b8f09e
Darya Karotchanka <daria.vyacheslavovna@gmail.com>
2021-07-11 20:39:01 +0200

    init: start server (first draft)

---
+++ english-for-kids-api/src/app.ts
@@ -0,0 +1,16 @@
+import path from 'path';
+import express from 'express';
+import cors from 'cors';
+import {json} from 'body-parser';
+import categories from './category/router';
+
+const staticFilesPath = path.resolve(__dirname, '../wwwroot');
+
+const app = express();
+// app.use(bodyParser.urlencoded({extended: false}))
+app.use(json());
+app.use(cors());
+app.use(/^(?!\/api\/)/, express.static(staticFilesPath));
+app.use('/api/categories', categories);
+
+app.listen(3000, () => console.log('server started on 3000'));
